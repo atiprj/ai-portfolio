5cbf782c6c20de64286e16de35f2e2a760805b58
mattia.giannetti <mattiagiannetti@atiproject.com>
2024-09-11 21:30:49 +0000

    Updates

--- _next/static/nhAl-afI07xoWloLO-p0i/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-1a32af3a6d3d5ae3.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
