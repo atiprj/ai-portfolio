20c97865d0121fd1c3e687af321ff8231b9c71a1
mattia.giannetti <mattiagiannetti@atiproject.com>
2024-09-13 10:09:50 +0000

    Updates

---
+++ _next/static/n4aVOk6absat-cTcUuqR9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/17-96be12847c4ff025.js","static/chunks/pages/index-4cb0edb7457ae5f5.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
