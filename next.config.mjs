3699fdeccdc186724519979790a45164488a7032
mattiagianne <56370685+mattiagianne@users.noreply.github.com>
2024-12-12 13:35:26 +0100

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true,
     domains: ['images.unsplash.com', 'example.com'],
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/ai-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ai-portfolio/' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/ai-portfolio' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/ai-portfolio/' : '',
   output: 'export',
 }
 
