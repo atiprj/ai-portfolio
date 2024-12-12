11e7f532a1bd7a44770be586a37673e6404be278
mattiagianne <56370685+mattiagianne@users.noreply.github.com>
2024-12-12 14:07:36 +0100

    Deploy on main in docs folder

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true,
     domains: ['images.unsplash.com', 'example.com'],
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/ai-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ai-portfolio/' : '',
+  basePath: '/ai-portfolio',
+  assetPrefix: '/ai-portfolio/',
   output: 'export',
 }
 
